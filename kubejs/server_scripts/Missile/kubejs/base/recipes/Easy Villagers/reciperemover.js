0d8d230185ba1abbdf31cde3fe78a2e9223495d5
MissileMann <misael.escoto043@gmail.com>
2022-01-17 09:07:15 -0600

    Add files via upload

---
+++ kubejs/server_scripts/Missile/kubejs/base/recipes/Easy Villagers/reciperemover.js
@@ -0,0 +1,9 @@
+onEvent('recipes', event => {
+    event.replaceInput({ mod: 'easy_villagers' }, '#forge:ingots/iron', '#forge:storage_blocks/iron')
+    //event.remove({ output: 'easy_villagers:auto_trader' })
+    //event.remove({ output: 'easy_villagers:farmer' })
+    //event.remove({ output: 'easy_villagers:breeder' })
+    //event.remove({ output: 'easy_villagers:converter' })
+    //event.remove({ output: 'easy_villagers:iron_farm' })
+    //event.remove({ output: 'easy_villagers:incubator' })
+})
