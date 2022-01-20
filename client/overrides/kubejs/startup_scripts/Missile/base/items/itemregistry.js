36ef58a2a511608bd4f672361c09d42e505f0dc0
MissileMann <misael.escoto043@gmail.com>
2022-01-20 12:59:43 -0600

    Added Cured Rubber Sheet to item registry

--- client/overrides/kubejs/startup_scripts/Missile/base/items/itemregistry.js
+++ client/overrides/kubejs/startup_scripts/Missile/base/items/itemregistry.js
@@ -1,4 +1,5 @@
 onEvent('item.registry', event => {
     event.create('thermal_mechanism').displayName('Thermal Mechanism')
     event.create('gun_part').displayName('Gun Part')
+    event.create('rubber_sheet').displayName('Cured Rubber Sheet')
 })
